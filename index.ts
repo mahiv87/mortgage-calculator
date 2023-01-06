ff50eef7c2f74abd89a6055be186d20083f0ea9e
Marcus mbp <maherreraiv1987@gmail.com>
2023-01-05 20:08:34 -0700

    added question for inquirer

--- index.ts
+++ index.ts
@@ -1,1 +1,19 @@
 import inquirer from 'inquirer';
+
+const questions = [
+	{
+		type: 'input',
+		name: 'principal',
+		message: 'What is the Principal?'
+	},
+	{
+		type: 'input',
+		name: 'apr',
+		message: 'What is the Annual Interest Rate?'
+	},
+	{
+		type: 'input',
+		name: 'term',
+		message: 'How long is the loan term (years)?'
+	}
+];
