a30aa04852ea2b4e56b674b093d765b0c4cbfa8f
Marcus mbp <maherreraiv1987@gmail.com>
2023-01-05 20:22:46 -0700

    fixed typo of MortgageResponse interface

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import inquirer from 'inquirer';
 
 // interface for Inquirer prompt response
-interface MortageResponse {
+interface MortgageResponse {
 	principal: string;
 	apr: string;
 	term: string;
@@ -27,11 +27,11 @@ const questions = [
 ];
 
 // This function right here will calculate the mortgage
-const calculateMortgage = (data: MortageResponse): string => data.principal;
+const calculateMortgage = (data: MortgageResponse): string => data.principal;
 
 // Function to ask question, and pass data to calculateMortgage()
 const mortgageQuestions = (): string => {
-	inquirer.prompt(questions).then((responses: MortageResponse) => {
+	inquirer.prompt(questions).then((responses: MortgageResponse) => {
 		// console.log(responses);
 		calculateMortgage(responses);
 	});
